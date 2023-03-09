b92d62b0e69fdeb46cec7dfee4af3c1f63154fdd
Mustafacoban <mustafa.coban@std.hku.edu.tr>
2023-03-10 00:50:27 +0300

    eighth practice

---
+++ Sql_practice8.sql
@@ -0,0 +1,31 @@
+--CREATE TABLE employee(
+--	id SERIAL PRIMARY KEY,
+--	first_name VARCHAR(50) NOT NULL,
+--	last_name VARCHAR(50) NOT NULL,
+--	birthday DATE,
+--	email VARCHAR(100)
+--);
+
+
+
+
+
+
+
+--Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+
+--UPDATE employee
+--SET first_name = 'mustafa', last_name = 'coban', birthday = '1999-03-04', email = 'mustafa@coban.com'
+--WHERE id=26;
+
+--UPDATE employee
+--SET first_name = 'Emir', last_name = 'Shepherd', birthday = '1989-03-04', email = 'emir@coban.com'
+--WHERE id BETWEEN 1 AND 5;
+
+
+--DELETE FROM employee WHERE id > 50;
+
+
+
+
+
